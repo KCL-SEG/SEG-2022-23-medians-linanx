123936483d7060e801881d0b8a2ac9e3a00a132e
Lina Namoun <76211897+linanx@users.noreply.github.com>
2022-09-23 21:54:27 +0100

    Completed task

--- median.py
+++ median.py
@@ -10,3 +10,13 @@ while True:
     else:
         break
 print(numbers)
+numbers.sort()
+print(f'Your sorted list: {numbers}')
+
+if len(numbers)%2 == 0:
+    m1 = (len(numbers)//2)-1
+    m2 = len(numbers)//2
+    print((numbers[m1]+numbers[m2])/2)
+else:
+    length = ((len(numbers)+1)//2)
+    print(numbers[length-1])
